62ebd7c003063e99adb32a67fd9d5c9cf0e5ab5d
Sunny <shan66@robolabs.org>
2024-07-26 19:07:36 -0700

    adding new repo

---
+++ src/main.cpp
@@ -0,0 +1,310 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Robolabs                                                  */
+/*    Created:      7/26/2024, 6:58:15 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+// A global instance of brain used for printing to the V5 Brain screen.
+motor leftDriveA = motor(PORT9, ratio6_1, true);
+motor leftDriveB = motor(PORT20, ratio6_1, false); 
+motor leftDriveC = motor(PORT5, ratio6_1, true);
+
+motor rightDriveA = motor(PORT10, ratio6_1, false);
+motor rightDriveB = motor(PORT6, ratio6_1, true); 
+motor rightDriveC = motor(PORT4, ratio6_1, false);
+
+inertial DrivetrainInertial = inertial(PORT8);
+
+controller Controller1 = controller(primary);
+brain Brain;
+
+motor Intake = motor(PORT1, ratio6_1, false);
+motor Arm = motor(PORT2, ratio6_1, true); 
+
+digital_out Claw = digital_out(Brain.ThreeWirePort.A);
+
+motor_group LeftDriveSmart = motor_group(leftDriveA, leftDriveB, leftDriveC);
+
+motor_group RightDriveSmart = motor_group(rightDriveA, rightDriveB, rightDriveC);
+smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 299.24, 320, 40, mm, 0.6);
+
+
+/*---------------------------------------------------------------------------*/
+/*                             JAR-Template Config                           */
+/*                                                                           */
+/*  Where all the magic happens. Follow the instructions below to input      */
+/*  all the physical constants and values for your robot. You should         */
+/*  already have configured your motors.                                     */
+/*---------------------------------------------------------------------------*/
+
+Drive chassis(
+
+//Pick your drive setup from the list below:
+//ZERO_TRACKER_NO_ODOM
+//ZERO_TRACKER_ODOM
+//TANK_ONE_FORWARD_ENCODER
+//TANK_ONE_FORWARD_ROTATION
+//TANK_ONE_SIDEWAYS_ENCODER
+//TANK_ONE_SIDEWAYS_ROTATION
+//TANK_TWO_ENCODER
+//TANK_TWO_ROTATION
+//HOLONOMIC_TWO_ENCODER
+//HOLONOMIC_TWO_ROTATION
+//
+//Write it here:
+TANK_TWO_ENCODER,
+
+//Add the names of your Drive motors into the motor groups below, separated by commas, i.e. motor_group(Motor1,Motor2,Motor3).
+//You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2"
+
+//Left Motors:
+LeftDriveSmart,
+
+//Right Motors:
+RightDriveSmart,
+
+//Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
+PORT4,
+
+//Input your wheel diameter. (4" omnis are actually closer to 4.125"):
+3.25,
+
+//External ratio, must be in decimal, in the format of input teeth/output teeth.
+//If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
+//If the motor drives the wheel directly, this value is 1:
+1.6666666666666666666666666666667,
+
+//Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
+//For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
+360,
+
+/*---------------------------------------------------------------------------*/
+/*                                  PAUSE!                                   */
+/*                                                                           */
+/*  The rest of the drive constructor is for robots using POSITION TRACKING. */
+/*  If you are not using position tracking, leave the rest of the values as  */
+/*  they are.                                                                */
+/*---------------------------------------------------------------------------*/
+
+//If you are using ZERO_TRACKER_ODOM, you ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
+
+//FOR HOLONOMIC DRIVES ONLY: Input your drive motors by position. This is only necessary for holonomic drives, otherwise this section can be left alone.
+//LF:      //RF:    
+PORT1,     -PORT2,
+
+//LB:      //RB: 
+PORT3,     -PORT4,
+
+//If you are using position tracking, this is the Forward Tracker port (the tracker which runs parallel to the direction of the chassis).
+//If this is a rotation sensor, enter it in "PORT1" format, inputting the port below.
+//If this is an encoder, enter the port as an integer. Triport A will be a "1", Triport B will be a "2", etc.
+PORT12, //Will do later
+
+//Input the Forward Tracker diameter (reverse it to make the direction switch):
+2.75,
+
+//Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
+//For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
+//This distance is in inches:
+0,
+
+//Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
+PORT11,
+
+//Sideways tracker diameter (reverse to make the direction switch):
+2,
+
+//Sideways tracker center distance (positive distance is behind the center of the robot, negative is in front):
+1.5
+
+);
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+  bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+  bool Claw_toggle = false; 
+
+
+
+  while (1) {
+    // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3
+      // right = Axis2
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position();
+      int drivetrainRightSideSpeed = Controller1.Axis2.position();
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
+      }
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+      if (Controller1.ButtonL1.pressing()) {
+        Intake.spin(fwd, 90, pct);
+      } else if (Controller1.ButtonL2.pressing()) {
+        Intake.spin(reverse, 90, pct);
+      } else {
+        Intake.stop();
+      }
+
+      if (Claw_toggle) {
+        Claw.set(true);
+      } else {
+        Claw.set(false);
+      }
+
+      if (Controller1.ButtonA.pressing()) {
+        Claw_toggle = !Claw_toggle;
+        waitUntil(!Controller1.ButtonA.pressing());
+      }
+
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    //Replace this line with chassis.control_tank(); for tank drive 
+    //or chassis.control_holonomic(); for holo drive.
+    chassis.control_arcade();
+
+
+    if(Controller1.ButtonL1.pressing()) {
+      Intake.spin(fwd, 100, pct);
+    } else if (Controller1.ButtonL2.pressing()) {
+      Intake.spin(reverse, 100, pct);
+    } else {
+      Intake.stop(brake); // change to hold or coast or brake
+    }
+
+  
+    if(Controller1.ButtonR1.pressing()) {
+      Arm.spin(fwd, 100, pct);
+    } else if (Controller1.ButtonR2.pressing()) {
+      Arm.spin(reverse, 100, pct);
+    } else {
+      Arm.stop(brake); // change to hold or coast or brake
+    }
+
+  }
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
